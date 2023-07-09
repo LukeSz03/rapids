cfc112a23be499168d4409e97d3c69b8f2d6ceb8
Simon Tran <simon.tran2@mail.mcgill.ca>
2023-07-09 18:40:42 -0400

    Add basic frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Rapids</h1>
+      <h2>A simple, and fast file sharing service</h2>
+      <img src="/logo.png" alt="logo" id="logo"></img>
+    </>
+  );
+}
+
+export default App;
