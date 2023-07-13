7e5df1387312b323011b74eb60a71abc67f95a7c
Simon Tran <simon.tran2@mail.mcgill.ca>
2023-07-12 22:59:51 -0400

    Move home to Home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
+import Home from './Home';
+
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <h1>Rapids</h1>
-          <h2>Upload files and share <br />  instantaneously</h2>
-          <button className="button">Upload file</button>
-        </div>
-        <img src="logo.png" alt="Rapids logo" id="logo" />
-      </div>
-      <footer>
-        <p>Made with <span class="heart">❤</span> by Simon Tran 🔗 Luke Schultz</p>
-      </footer>
-    </ >
+      <Home />
+    </>
   );
 }
 
