fb0f2c37e8977f8589496313335b484e4f8566d8
Simon Tran <simon.tran2@mail.mcgill.ca>
2023-07-15 16:53:27 -0400

    Add ui for upload

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -1,13 +1,27 @@
 import "./Upload.css";
+import { useState } from 'react';
 
 function Upload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileInputChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
   return (
     <div className="center">
-      <h1>Test</h1>
+      <div className="dropzone-border">
+        <div className="dropzone" onClick={() => document.getElementById('fileInput').click()}>
+          <img src="./upload-icon.png" alt="upload-icon" width="100px" />
+          <p>Click or drag and drop to upload files</p>
+          <input type="file" id="fileInput" onChange={handleFileInputChange} style={{ display: 'none' }} />
+        </div>
+      </div>
+      <button className="send button" disabled={!selectedFile}>
+          Create link
+      </button>
     </div>
   );
 }
 
 export default Upload;
-
-// ⬆ Upload files
