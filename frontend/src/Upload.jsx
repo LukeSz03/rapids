3e2899e4d50dc2bf328b26b4402ccf3747843835
Luke Schultz <lukeschultz31@gmail.com>
2023-07-16 15:38:28 -0400

    Implemented File Drag and Drop

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -2,23 +2,39 @@ import "./Upload.css";
 import { useState } from 'react';
 
 function Upload() {
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFiles, setSelectedFiles] = useState([]);
 
-  const handleFileInputChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+  const handleImport = (event) => {
+    const newFiles = Array.from(event.target.files);
+    setSelectedFiles(selectedFiles.concat(newFiles));
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const newFiles = Array.from(event.dataTransfer.files);
+    setSelectedFiles(selectedFiles.concat(newFiles));
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  const handleButtonClick = () => {
+    console.log(selectedFiles); //for testing purpose
   };
 
   return (
     <div className="center">
       <div className="dropzone-border">
-        <div className="dropzone" onClick={() => document.getElementById('fileInput').click()}>
+        <div className="dropzone" onClick={() => document.getElementById('fileInput').click()} onDrop={handleDrop} onDragOver={handleDragOver}>
           <img src="./upload-icon.png" alt="upload-icon" width="100px" />
           <p>Click or drag and drop to upload files</p>
-          <input type="file" id="fileInput" onChange={handleFileInputChange} style={{ display: 'none' }} />
+          <input type="file" id="fileInput" onChange={handleImport} style={{ display: 'none' }} multiple /> 
         </div>
       </div>
-      <button className="send button" disabled={!selectedFile}>
-          Create link
+      <button className="send button" onClick={handleButtonClick}>
+        Create link
       </button>
     </div>
   );
